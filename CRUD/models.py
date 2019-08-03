0200fa441f280788027bb2c14732ecf69be7632a
Mcanthony G <mcanthony.gonsalves@parinati.in>
2019-08-03 16:20:28 +0530

    length changed

--- CRUD/models.py
+++ CRUD/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class BookList(models.Model):
 	title=models.CharField(max_length=200)
 	price=models.IntegerField()
-	author=models.CharField(max_length=100)
+	author=models.CharField(max_length=500)
 
 	
